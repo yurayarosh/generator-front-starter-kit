9256f8b7480ea8880813cea2cdb6b9db76c2af4c
Yurii Yaroshenko <yura12323@gmail.com>
2020-06-24 17:20:40 +0300

    Update `BEMBlock()`

--- generators/app/templates/src/js/lib/BEMBlock.js
+++ generators/app/templates/src/js/lib/BEMBlock.js
@@ -1,18 +1,17 @@
 const BEMblock = (node, block = '') => {
   if (!block) {
     let classes = node.className.split(' ')
-    if (classes.length === 1) {
-      ;[block] = classes
-    } else if (classes.length > 0) {
-      const hasJsClassName = classes.filter(name => name.indexOf('js-') === 0).length > 0
 
-      classes = classes.filter(name =>
-        hasJsClassName
-          ? !(name.indexOf('js-') !== -1 || name.indexOf('--') !== -1)
-          : !(name.indexOf('js-') !== -1 || name.indexOf('--') !== -1 || name.indexOf('__') !== -1)
-      )
+    if (classes.length > 0) {
+      classes = classes.filter(cl => cl !== '')
 
-      classes.length !== 1 ? (block = '') : ([block] = classes)
+      if (classes.length === 1) {
+        ;[block] = classes
+      } else if (classes.length > 0) {
+        classes = classes.filter(name => !(name.indexOf('js-') !== -1 || name.indexOf('--') !== -1))
+
+        classes.length !== 1 ? (block = '') : ([block] = classes)
+      }
     }
   }
 
