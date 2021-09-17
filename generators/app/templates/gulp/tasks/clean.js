3b8f43ea48607e86644f9590ac133dc7a2268a56
Yurii Yaroshenko <yura12323@gmail.com>
2021-09-17 10:50:17 +0300

    Gulp `clean.js` task update.

--- generators/app/templates/gulp/tasks/clean.js
+++ generators/app/templates/gulp/tasks/clean.js
@@ -3,8 +3,9 @@ import colors from 'ansi-colors'
 import log from 'fancy-log'
 import { dest } from '../config'
 
-const build = () => () => {
-  return del([dest.root]).then(paths => log('Deleted:', colors.magenta(paths.join('\n'))))
+const build = () => async () => {
+  const paths = await del([dest.root])
+  return log('Deleted:', colors.magenta(paths.join('\n')))
 }
 
 module.exports.build = build
